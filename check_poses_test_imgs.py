2a7242730b8d10416a02a3268148fc417035779f
MathGaron <mathieugaron91@gmail.com>
2017-06-08 16:25:54 -0400

    load configuration yaml

--- check_poses_test_imgs.py
+++ check_poses_test_imgs.py
@@ -10,123 +10,141 @@ import os
 import numpy as np
 import scipy.misc
 import matplotlib.pyplot as plt
+import click
+import yaml
 
-scene_ids = range(1, 21)
-device = 'primesense' # options: 'primesense', 'kinect', 'canon'
-model_type = 'cad' # options: 'cad', 'reconst'
-im_step = 100 # Consider every im_step-th image
 
-# Path to the T-LESS dataset
-data_path = '/media/tom/T-LESS/t-less/data_release/t-less_v2'
+@click.group()
+@click.option('--config', default='config.yml')
+@click.pass_context
+def main(ctx, config):
+    file = open(config)
+    config = yaml.safe_load(file)
+    file.close()
+    ctx.obj['config'] = config
 
-# Path to the folder in which the images produced by this script will be saved
-output_dir = os.path.join(data_path, 'output_check_poses_test_imgs')
 
-# Paths to the elements of the T-LESS dataset
-model_path_mask = os.path.join(data_path, 'models_' + model_type, 'obj_{:02d}.ply')
-scene_info_path_mask = os.path.join(data_path, 'test_{}', 'scene_{:02d}', 'scene_info.yml')
-scene_gt_path_mask = os.path.join(data_path, 'test_{}', 'scene_{:02d}', 'scene_gt.yml')
-rgb_path_mask = os.path.join(data_path, 'test_{}', 'scene_{:02d}', 'rgb', '{:04d}.{}')
-depth_path_mask = os.path.join(data_path, 'test_{}', 'scene_{:02d}', 'depth', '{:04d}.png')
-rgb_ext = {'primesense': 'png', 'kinect': 'png', 'canon': 'jpg'}
-obj_colors_path = os.path.join('data', 'obj_rgb.txt')
-vis_rgb_path_mask = os.path.join(output_dir, 'scene_{:02d}_{}_{}_{:04d}_rgb.png')
-vis_depth_path_mask = os.path.join(output_dir, 'scene_{:02d}_{}_{}_{:04d}_depth_diff.png')
+@main.command(
+    help='Test generate poses rendering over original frames.')
+@click.pass_context
+def test(ctx):
+    config = ctx.obj['config']
+    scene_ids = range(1, 21)
+    device = config['device']
+    model_type = config['model_type']
+    im_step = int(config['image_step'])
 
-misc.ensure_dir(output_dir)
-obj_colors = inout.load_colors(obj_colors_path)
+    data_path = config['dataset_path']
+    output_dir = config['output_path']
+    # Paths to the elements of the T-LESS dataset
+    model_path_mask = os.path.join(data_path, 'models_' + model_type, 'obj_{:02d}.ply')
+    scene_info_path_mask = os.path.join(data_path, 'test_{}', '{:02d}', 'info.yml')
+    scene_gt_path_mask = os.path.join(data_path, 'test_{}', '{:02d}', 'gt.yml')
+    rgb_path_mask = os.path.join(data_path, 'test_{}', '{:02d}', 'rgb', '{:04d}.{}')
+    depth_path_mask = os.path.join(data_path, 'test_{}', '{:02d}', 'depth', '{:04d}.png')
+    rgb_ext = {'primesense': 'png', 'kinect': 'png', 'canon': 'jpg'}
+    obj_colors_path = os.path.join('data', 'obj_rgb.txt')
+    vis_rgb_path_mask = os.path.join(output_dir, '{:02d}_{}_{}_{:04d}_rgb.png')
+    vis_depth_path_mask = os.path.join(output_dir, '{:02d}_{}_{}_{:04d}_depth_diff.png')
 
-plt.ioff() # Turn interactive plotting off
+    misc.ensure_dir(output_dir)
+    obj_colors = inout.load_colors(obj_colors_path)
 
-for scene_id in scene_ids:
+    plt.ioff()  # Turn interactive plotting off
 
-    # Load info about the test images (including camera parameters etc.)
-    scene_info_path = scene_info_path_mask.format(device, scene_id)
-    scene_info = inout.load_scene_info(scene_info_path)
+    for scene_id in scene_ids:
 
-    scene_gt_path = scene_gt_path_mask.format(device, scene_id)
-    gts = inout.load_scene_gt(scene_gt_path)
+        # Load info about the test images (including camera parameters etc.)
+        scene_info_path = scene_info_path_mask.format(device, scene_id)
+        scene_info = inout.load_scene_info(scene_info_path)
 
-    # Load models of objects present in the scene
-    scene_obj_ids = set()
-    for gt in gts[0]:
-        scene_obj_ids.add(gt['obj_id'])
-    models = {}
-    for scene_obj_id in scene_obj_ids:
-        model_path = model_path_mask.format(scene_obj_id)
-        models[scene_obj_id] = inout.load_ply(model_path)
+        scene_gt_path = scene_gt_path_mask.format(device, scene_id)
+        gts = inout.load_scene_gt(scene_gt_path)
 
-    for im_id, im_info in scene_info.items():
-        if im_id % im_step != 0:
-            continue
-        print('scene: ' + str(scene_id) + ', device: ' + device + ', im_id: ' + str(im_id))
+        # Load models of objects present in the scene
+        scene_obj_ids = set()
+        for gt in gts[0]:
+            scene_obj_ids.add(gt['obj_id'])
+        models = {}
+        for scene_obj_id in scene_obj_ids:
+            model_path = model_path_mask.format(scene_obj_id)
+            models[scene_obj_id] = inout.load_ply(model_path)
 
-        # Get intrinsic camera parameters
-        K = im_info['cam_K']
+        for im_id, im_info in scene_info.items():
+            if im_id % im_step != 0:
+                continue
+            print('scene: ' + str(scene_id) + ', device: ' + device + ', im_id: ' + str(im_id))
 
-        # Visualization #1
-        #-----------------------------------------------------------------------
-        # Load RGB image
-        rgb_path = rgb_path_mask.format(device, scene_id, im_id, rgb_ext[device])
-        rgb = scipy.misc.imread(rgb_path)
+            # Get intrinsic camera parameters
+            K = im_info['cam_K']
 
-        im_size = (rgb.shape[1], rgb.shape[0])
-        vis_rgb = np.zeros(rgb.shape, np.float)
-        for gt in gts[im_id]:
-            model = models[gt['obj_id']]
-            R = gt['cam_R_m2c']
-            t = gt['cam_t_m2c']
-            surf_color = obj_colors[gt['obj_id'] - 1]
+            # Visualization #1
+            # -----------------------------------------------------------------------
+            # Load RGB image
+            rgb_path = rgb_path_mask.format(device, scene_id, im_id, rgb_ext[device])
+            rgb = scipy.misc.imread(rgb_path)
 
-            ren_rgb = renderer.render(model, im_size, K, R, t,
-                                      surf_color=surf_color, mode='rgb')
-
-            # Draw the bounding box of the object
-            ren_rgb = misc.draw_rect(ren_rgb, gt['obj_bb'])
-
-            vis_rgb += 0.7 * ren_rgb.astype(np.float)
-
-        # Save the visualization
-        vis_rgb = 0.6 * vis_rgb + 0.4 * rgb
-        vis_rgb[vis_rgb > 255] = 255
-        vis_rgb_path = vis_rgb_path_mask.format(scene_id, device, model_type, im_id)
-        scipy.misc.imsave(vis_rgb_path, vis_rgb.astype(np.uint8))
-
-        # Visualization #2
-        #-----------------------------------------------------------------------
-        if device != 'canon':
-            # Load depth image
-            depth_path = depth_path_mask.format(device, scene_id, im_id, rgb_ext[device])
-            depth = scipy.misc.imread(depth_path)  # Unit: 0.1 mm
-            depth = depth.astype(np.float) * 0.1  # Convert to mm
-
-            # Render the objects at the ground truth poses
-            im_size = (depth.shape[1], depth.shape[0])
-            ren_depth = np.zeros(depth.shape, np.float)
+            im_size = (rgb.shape[1], rgb.shape[0])
+            vis_rgb = np.zeros(rgb.shape, np.float)
             for gt in gts[im_id]:
                 model = models[gt['obj_id']]
                 R = gt['cam_R_m2c']
                 t = gt['cam_t_m2c']
+                surf_color = obj_colors[gt['obj_id'] - 1]
 
-                # Render the current object
-                ren_depth_obj = renderer.render(model, im_size, K, R, t, mode='depth')
+                ren_rgb = renderer.render(model, im_size, K, R, t,
+                                          surf_color=surf_color, mode='rgb')
 
-                # Add to the final depth map only the parts of the surface that
-                # are closer than the surfaces rendered before
-                visible_mask = np.logical_or(ren_depth == 0, ren_depth_obj < ren_depth)
-                mask = np.logical_and(ren_depth_obj != 0, visible_mask)
-                ren_depth[mask] = ren_depth_obj[mask].astype(np.float)
+                # Draw the bounding box of the object
+                ren_rgb = misc.draw_rect(ren_rgb, gt['obj_bb'])
 
-            # Calculate the depth difference at pixels where both depth maps
-            # are valid
-            valid_mask = (depth > 0) * (ren_depth > 0)
-            depth_diff = valid_mask * (depth - ren_depth.astype(np.float))
+                vis_rgb += 0.7 * ren_rgb.astype(np.float)
 
             # Save the visualization
-            vis_depth_path = vis_depth_path_mask.format(scene_id, device,
-                                                        model_type, im_id)
-            plt.matshow(depth_diff)
-            plt.title('captured - rendered depth [mm]')
-            plt.colorbar()
-            plt.savefig(vis_depth_path, pad=0)
-            plt.close()
+            vis_rgb = 0.6 * vis_rgb + 0.4 * rgb
+            vis_rgb[vis_rgb > 255] = 255
+            vis_rgb_path = vis_rgb_path_mask.format(scene_id, device, model_type, im_id)
+            scipy.misc.imsave(vis_rgb_path, vis_rgb.astype(np.uint8))
+
+            # Visualization #2
+            # -----------------------------------------------------------------------
+            if device != 'canon':
+                # Load depth image
+                depth_path = depth_path_mask.format(device, scene_id, im_id, rgb_ext[device])
+                depth = scipy.misc.imread(depth_path)  # Unit: 0.1 mm
+                depth = depth.astype(np.float) * 0.1  # Convert to mm
+
+                # Render the objects at the ground truth poses
+                im_size = (depth.shape[1], depth.shape[0])
+                ren_depth = np.zeros(depth.shape, np.float)
+                for gt in gts[im_id]:
+                    model = models[gt['obj_id']]
+                    R = gt['cam_R_m2c']
+                    t = gt['cam_t_m2c']
+
+                    # Render the current object
+                    ren_depth_obj = renderer.render(model, im_size, K, R, t, mode='depth')
+
+                    # Add to the final depth map only the parts of the surface that
+                    # are closer than the surfaces rendered before
+                    visible_mask = np.logical_or(ren_depth == 0, ren_depth_obj < ren_depth)
+                    mask = np.logical_and(ren_depth_obj != 0, visible_mask)
+                    ren_depth[mask] = ren_depth_obj[mask].astype(np.float)
+
+                # Calculate the depth difference at pixels where both depth maps
+                # are valid
+                valid_mask = (depth > 0) * (ren_depth > 0)
+                depth_diff = valid_mask * (depth - ren_depth.astype(np.float))
+
+                # Save the visualization
+                vis_depth_path = vis_depth_path_mask.format(scene_id, device,
+                                                            model_type, im_id)
+                plt.matshow(depth_diff)
+                plt.title('captured - rendered depth [mm]')
+                plt.colorbar()
+                plt.savefig(vis_depth_path, pad=0)
+                plt.close()
+
+
+if __name__ == '__main__':
+    main(obj={})
